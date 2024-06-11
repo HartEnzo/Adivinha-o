a6f440cf1739b1454da699b6c599feb239c7fe67
Enzo Guilherme <enzoguihartmann@gmail.com>
2024-06-11 11:23:21 -0300

    init

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const aleatorio = Math.floor(Math.random() * 10 + 1);
+const aleatorio = Math.floor(Math.random() * 200 + 100);
 console.log("Digite um número:");
 const Max_tentativas = 5;
 let tentativas = 0;
